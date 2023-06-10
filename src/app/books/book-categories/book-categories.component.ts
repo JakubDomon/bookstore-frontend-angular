88f038531ec527f1abfce3065cc1860653a9e0fd
JakubDomon <jakubdomonn@gmail.com>
2023-06-10 09:32:31 +0200

    Book categories component

---
+++ src/app/books/book-categories/book-categories.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-book-categories',
+  templateUrl: './book-categories.component.html',
+  styleUrls: ['./book-categories.component.css']
+})
+export class BookCategoriesComponent {
+
+}
