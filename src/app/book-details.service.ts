77abb834cde935bf3c29c96afd8d5113e31604a0
JakubDomon <jakubdomonn@gmail.com>
2023-06-10 12:25:42 +0200

    Book details service fake content

--- src/app/book-details.service.ts
+++ src/app/book-details.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookDetailsService {
-
-  constructor() { }
+  getBookDetails(bookID: number){
+    return of([
+      { id: 1, title: 'Title1', description: 'desc1', author: 'marian paździoch', opinions: [
+        {id: 1, author: 'ferdynand kiepski', text: 'asasdasdasdasdasdasdasd'}
+      ]},
+      { id: 2, title: 'Title2', description: 'desc2', author: 'marian paździoch2', opinions: [
+        {id: 1, author: 'ferdynand kiepski', text: 'asasdasdasdasdasdasdasd'}
+      ]},
+      { id: 3, title: 'Title3', description: 'desc3', author: 'marian paździoch3', opinions: [
+        {id: 1, author: 'ferdynand kiepski', text: 'asasdasdasdasdasdasdasd'}
+      ]},
+    ])
+  }
 }
