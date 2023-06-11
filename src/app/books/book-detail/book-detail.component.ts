4ec9f166c12d45bcc20ca751e5aa241971530354
JakubDomon <jakubdomonn@gmail.com>
2023-06-11 18:30:25 +0200

    Layout changes

--- src/app/books/book-detail/book-detail.component.ts
+++ src/app/books/book-detail/book-detail.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BookDetailsService } from 'src/app/book-details.service';
 
 @Component({
   selector: 'app-book-detail',
   templateUrl: './book-detail.component.html',
   styleUrls: ['./book-detail.component.css']
 })
-export class BookDetailComponent {
+export class BookDetailComponent implements OnInit{
+  book: any
+
+  constructor(private BookDetailsService: BookDetailsService){};
+
+  ngOnInit(): void {
+    // Book details
+    this.BookDetailsService
+    .getBookDetails(1)
+    .subscribe({
+      next: book => {
+        this.book = book
+        console.log(book)
+      }
+    })
+  }
 
 }
