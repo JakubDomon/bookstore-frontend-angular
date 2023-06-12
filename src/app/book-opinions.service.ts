8aca3911fa41f2693f190e6335a68bb323d5b12d
JakubDomon <jakubdomonn@gmail.com>
2023-06-12 20:27:26 +0200

    Book detail view

--- src/app/book-opinions.service.ts
+++ src/app/book-opinions.service.ts
@@ -8,12 +8,12 @@ export class BookOpinionsService {
 
   getBookOpinions(bookId: number){
     return of([
-      { id: 1, author: 'Jakub Domoń', text: 'asdadsasdadsasd', rating: 4 },
-      { id: 1, author: 'Jakub Domoń', text: 'asdadsasdadsasd', rating: 4 },
-      { id: 1, author: 'Jakub Domoń', text: 'asdadsasdadsasd', rating: 4 },
-      { id: 1, author: 'Jakub Domoń', text: 'asdadsasdadsasd', rating: 4 },
-      { id: 1, author: 'Jakub Domoń', text: 'asdadsasdadsasd', rating: 4 },
-      { id: 1, author: 'Jakub Domoń', text: 'asdadsasdadsasd', rating: 4 },
+      { id: 1, author: 'Jakub Domoń', authorImage: 'assets/images/books/cat.png', title: 'testss', text: 'asdadsasdadsasd', rating: 4 },
+      { id: 1, author: 'Jakub Domoń', authorImage: 'assets/images/books/cat.png', title: 'testss', text: 'asdadsasdadsasd', rating: 4 },
+      { id: 1, author: 'Jakub Domoń', authorImage: 'assets/images/books/cat.png', title: 'testss', text: 'asdadsasdadsasd', rating: 4 },
+      { id: 1, author: 'Jakub Domoń', authorImage: 'assets/images/books/cat.png', title: 'testss', text: 'asdadsasdadsasd', rating: 4 },
+      { id: 1, author: 'Jakub Domoń', authorImage: 'assets/images/books/cat.png', title: 'testss', text: 'asdadsasdadsasd', rating: 4 },
+      { id: 1, author: 'Jakub Domoń', authorImage: 'assets/images/books/cat.png', title: 'testss', text: 'asdadsasdadsasd', rating: 4 },
     ])
   }
 }
--- src/app/books/book-detail/book-opinions/book-opinions.component.ts
+++ src/app/books/book-detail/book-opinions/book-opinions.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BookOpinionsService } from 'src/app/book-opinions.service';
 
 @Component({
   selector: 'app-book-opinions',
   templateUrl: './book-opinions.component.html',
   styleUrls: ['./book-opinions.component.css']
 })
-export class BookOpinionsComponent {
+export class BookOpinionsComponent implements OnInit{
+  bookOpinions: any[] = [];
+
+  constructor(private bookOpinionsService: BookOpinionsService){}
+
+  ngOnInit(): void {
+    this.bookOpinionsService
+      .getBookOpinions(1)
+      .subscribe({
+        next: opinions => this.bookOpinions = opinions
+      })
+  }
 
 }
