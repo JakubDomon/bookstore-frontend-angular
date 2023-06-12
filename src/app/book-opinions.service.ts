c5866d34301b5f7c18197c648cf685a8c21f1cb8
JakubDomon <jakubdomonn@gmail.com>
2023-06-12 18:51:09 +0200

    Book opinion service

--- src/app/book-opinions.service.ts
+++ src/app/book-opinions.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookOpinionsService {
 
-  constructor() { }
+  getBookOpinions(bookId: number){
+    return of([
+      { id: 1, author: 'Jakub Domoń', text: 'asdadsasdadsasd', rating: 4 },
+      { id: 1, author: 'Jakub Domoń', text: 'asdadsasdadsasd', rating: 4 },
+      { id: 1, author: 'Jakub Domoń', text: 'asdadsasdadsasd', rating: 4 },
+      { id: 1, author: 'Jakub Domoń', text: 'asdadsasdadsasd', rating: 4 },
+      { id: 1, author: 'Jakub Domoń', text: 'asdadsasdadsasd', rating: 4 },
+      { id: 1, author: 'Jakub Domoń', text: 'asdadsasdadsasd', rating: 4 },
+    ])
+  }
 }
