5c32c1d6b29c07ef00578f94a15f3cfbaba92a0b
JakubDomon <jakubdomonn@gmail.com>
2023-06-12 18:46:41 +0200

    Book detail view

---
+++ src/app/books/book-detail/book-opinions/book-opinions.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-book-opinions',
+  templateUrl: './book-opinions.component.html',
+  styleUrls: ['./book-opinions.component.css']
+})
+export class BookOpinionsComponent {
+
+}
